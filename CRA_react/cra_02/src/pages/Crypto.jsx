a22444f0ab2b1b6ac4584e205e7f437c43f6ad7f
hyebinrk <gpqls3698@naver.com>
2025-06-10 17:29:34 +0900

    250610

--- CRA_react/cra_02/src/pages/Crypto.jsx
+++ CRA_react/cra_02/src/pages/Crypto.jsx
@@ -7,15 +7,22 @@ const Cryp = () => {
     const [inpw, setInpw] = useState('')
     const [skey, setSkey] = useState('')
 
-    const [encrypt, setEncrypt] = useState('')
-    const [decrypt, setDecrypt] = useState('')
+    const [encrypt, setEncrypt] = useState('') // 암호화
+    const [decrypt, setDecrypt] = useState('') // 복호화
+    const [sha, setSha] = useState('') // 단방향
 
+    // 암호화
+    // 암호화는 문자열만 처리할 수 있음. 객체는 암호화 할 수 없기 때문에 JSON.stringify()사용
     const encryptFn = () => {
         const data = { id: inid, pw: inpw }
         const encData = Crypto.AES.encrypt(JSON.stringify(data), skey).toString() // skey와 함께 데이터를 암호화
         setEncrypt(encData)
     }
 
+    // 복호화
+    // 복호화된 결과는 문자열 형태로 나옴. 이 문자열을 다시 객체로 돌리고 싶을 때 JSON.pars()함수 사용
+    // 복호화한 데이터를 실제로 쓰기 위해서는 문자열상태보다는 객체 상태가 JS에서 다루기 더 편하기 때문
+    // 복호화 한 값을 객체로 안 바꾸고 문자열로 쓰면 .id ,pw 접근이 안 됨
     const decryptFn = () => {
         try {
             const bytes = Crypto.AES.decrypt(encrypt, skey)
@@ -26,6 +33,13 @@ const Cryp = () => {
         } finally {
             alert('암호키 관리에 주의하세요.') // finally 무조건 뜨는 것!!
         }
+    }
+
+    // 단방향 해시값
+    const sha256fn = () => {
+        const data = { id: inid, pw: inpw } // 원하는 형태로 객체화
+        const encData = Crypto.SHA256(JSON.stringify(data)).toString() // skey와 함께 데이터를 암호화
+        setSha(encData)
     }
 
     // JSON.stringify({ id: inid, pw: inpw })
@@ -46,7 +60,8 @@ const Cryp = () => {
             <div>
                 Key : <input type="password" onChange={handleKeyChange} value={skey} />
             </div>
-            <button onClick={encryptFn}>암호화 동작</button>
+            <button onClick={encryptFn} disabled={!(inid && inpw && skey)}>암호화 동작</button> {/* 하나라도 빠지면 비활성화되게 */}
+
             <hr />
             <div>암호화 전: {(inid && inpw) && JSON.stringify({ id: inid, pw: inpw })}</div><br />
             <div> {!!encrypt && `암호화 후: ${encrypt} (글자수: ${encrypt.length})`}</div> <hr />
@@ -54,9 +69,14 @@ const Cryp = () => {
             <div>복호화 암호키
                 <input type="password" onChange={handleKeyChange} value={skey} />
             </div>
-            <button onClick={decryptFn}>복호화 동작</button>
+            <button onClick={decryptFn} disabled={!(inid && inpw && skey)}>복호화 동작</button>
+
+            {/* decrypt && JSON.stringify(decrypt) 이렇게만 사용해도 되지만 앞에 !!를 붙여줌으로써 좀 더 명시적으로 true와 false라는 의도를 나타냄. */}
             <div>{!!decrypt && JSON.stringify(decrypt)}</div> {/* decrypt값이 존재할 때 나타나고 없으면 안 나타남 */}
-            <div>{`복호화 된 데이터: ID는 ${decrypt.id}이고, PASSWORD는 ${decrypt.pw}입니다.`}</div>
+            <div>{!!decrypt && `복호화 된 데이터: ID는 ${decrypt.id}이고, PASSWORD는 ${decrypt.pw}입니다.`}</div>
+            <hr />
+            <button onClick={sha256fn} disabled={!(inid && inpw && skey)}>단방향 암호화 하기</button>
+            <h3>{`SHA256암호화: ${sha} (길이:${sha.length})`}</h3>
         </>
     )
 }
