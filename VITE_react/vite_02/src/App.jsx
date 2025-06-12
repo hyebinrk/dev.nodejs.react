da0acd2ea15eec5c4bb3b583605c02ce8e0f3b53
hyebinrk <gpqls3698@naver.com>
2025-06-12 10:51:21 +0900

    250612

--- VITE_react/vite_02/src/App.jsx
+++ VITE_react/vite_02/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 
 import Home from './pages/Home'
 import Css from './pages/Ex07'
+import TodoList from './pages/Ex08'
+import Eff from './pages/Ex09'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,11 +13,15 @@ function App() {
   return (
     <>
       <Link to="/">Home</Link> | {" "}
-      <Link to="/ex07">Home</Link> | {" "}
+      <Link to="/ex07">예제7</Link> | {" "}
+      <Link to="/ex08">예제8</Link> | {" "}
+      <Link to="/ex09">예제9</Link> | {" "}
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ex07" element={<Css />} />
+        <Route path="/ex08" element={<TodoList />} />
+        <Route path="/ex09" element={<Eff />} />
       </Routes>
     </>
   )
--- VITE_react/vite_02/src/pages/Ex07.jsx
+++ VITE_react/vite_02/src/pages/Ex07.jsx
@@ -1,6 +1,10 @@
 import "./Ex07.css"
+import myStyle2 from "./Ex07.module.css"
 const Css = () => {
     const myStyle1 = { width: "200px", height: "200px", borderRadius: "50%", backgroundColor: "Green" }
+
+
+
     return (
         <>
             <h1>6. 리액트에 CSS 스타일 적용하기</h1>
@@ -8,9 +12,15 @@ const Css = () => {
             <div style={myStyle1}></div>
             <div style={{ ...myStyle1, backgroundColor: "blue" }}></div>
             <div className="ex7-div"></div>
-            <div className={ }></div>
+            <ul>
+                <li><mark>아래는 모듈형 스타일 적용</mark></li>
+            </ul>
+            <div className={myStyle2["ex7-div"]}></div>
+            <ul>
+                <li>저도 적용됨</li>
+            </ul>
+            <div className={myStyle2["ex7ex7"]}></div>
         </>
     )
 }
-
 export default Css
